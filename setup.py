6691c276af75bda870d08bf2a29d9b2ff8edbc38
cheerzhang <zhanglenus@gmail.com>
2024-02-16 12:42:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='nl-ego-cities',
-    version='0.1.15',
+    version='0.1.16',
     author="L.Z",
     author_email="zhangle@gmail.com",
     description="Data for Netherland Cities Coordinates",
